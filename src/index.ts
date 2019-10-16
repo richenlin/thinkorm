6c3ab0385cb9c87f0aea2ba3ca1b75f347d1ee08
richen <richenlin@gmail.com>
2019-10-16 11:35:30 +0800

    Use typescript

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@
  * @ author: richen
  * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
  * @ license: MIT
- * @ version: 2019-10-16 10:15:55
+ * @ version: 2019-10-16 11:34:36
  */
 import * as think_lib from "think_lib";
 
 export const helper = think_lib;
 
 export * from "./BaseModel";
+export * from "./RelModel";
