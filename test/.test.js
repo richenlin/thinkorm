58c0e3644fdc064a383fb5552e22859f9d033e5e
richen <richenlin@gmail.com>
2019-10-17 11:08:25 +0800

    Fixbug

--- test/.test.js
+++ test/.test.js
@@ -6,7 +6,7 @@
  * @version    16/7/26
  */
 const path = require('path');
-const thinkorm = require('../index.js');
+const thinkorm = require('../dist/index.js');
 
 const config = {
     db_type: 'mysql',
@@ -23,7 +23,7 @@ const config = {
     // db_pwd: '',
     db_prefix: '',
     db_charset: 'utf8',
-    db_ext_config: {safe: false, db_log_sql: true, db_pool_size: 10, read_write: false}
+    db_ext_config: { safe: false, db_log_sql: true, db_pool_size: 10, read_write: false }
 };
 process.env.NODE_ENV = 'development';
 //thinkorm.require需要使用绝对路径
@@ -36,48 +36,48 @@ const model = new User(config);
 let now = Date.now();
 //查询语言测试
 return model
-// .migrate()
-// .where({id: {'<>': 1, '>=': 0}, name: 'rrrrrrr', or: [{name: 'aa'}, {name: 'aaa'}], not: {name: 1, id: 2}, notin: {name: [1,2,3]}}).find()
-// .where({or: [{name: {'like': '%aa%'}}, {memo: {'like': '%aa%'}}]}).find()
-// .where({id: {'>=': 0}}).count()
-// .where({id: {'>=': 0}}).sum('id')
-// .where({id: {'>=': 0}}).select()
-// .where({name: {'like': 'r%'}}).find()
-// .where({not: {name: 'rrrrrrrrrrrrr', id: 1}}).select()
-// .where({notin: {'id': [1,2,3]}}).select()
-// .where({name: {'like': '%a'}}).select()
-// .where({id: [1,2,3]}).select()
+    // .migrate()
+    // .where({id: {'<>': 1, '>=': 0}, name: 'rrrrrrr', or: [{name: 'aa'}, {name: 'aaa'}], not: {name: 1, id: 2}, notin: {name: [1,2,3]}}).find()
+    // .where({or: [{name: {'like': '%aa%'}}, {memo: {'like': '%aa%'}}]}).find()
+    // .where({id: {'>=': 0}}).count()
+    // .where({id: {'>=': 0}}).sum('id')
+    // .where({id: {'>=': 0}}).select()
+    // .where({name: {'like': 'r%'}}).find()
+    // .where({not: {name: 'rrrrrrrrrrrrr', id: 1}}).select()
+    // .where({notin: {'id': [1,2,3]}}).select()
+    // .where({name: {'like': '%a'}}).select()
+    // .where({id: [1,2,3]}).select()
 
-// .where({id: {'<>': 1, '>=': 0, notin: [1,2,3]}, name: ['aa', 'rrrrrrr'], notin: {'id': [1,2,3], num: [1,2,3]}, not: {name: '', num: [1,2,3]}, memo: {'like': '%a'}, or: [{name: 'aa', id: 1}, {name: 'rrrrrrr', id: {'>': 1}}]}).find()
-// .where({'and': {id: 1, name: 'aa'}}).find()//and做key
-// .where({or: [{id: 1, name: {or: [{name: 'aa'}, {memo: 'aa'}]}}, {profile: 'aa'}]}).find()//or嵌套
-// .where({in: {id: [1,2,3], num: [2,3]}}).find()//in做key
-// .where({'operator': {id: {'<>': 1, '>=': 0}}}).find()//operator做key
-// .select({field: 'id', limit: 1, order: {id: 'desc'}, where: {name: {'<>': '', not: 'aa', notin: ['aa', 'rrr'], like: '%a'}}}) //options高级用法
+    // .where({id: {'<>': 1, '>=': 0, notin: [1,2,3]}, name: ['aa', 'rrrrrrr'], notin: {'id': [1,2,3], num: [1,2,3]}, not: {name: '', num: [1,2,3]}, memo: {'like': '%a'}, or: [{name: 'aa', id: 1}, {name: 'rrrrrrr', id: {'>': 1}}]}).find()
+    // .where({'and': {id: 1, name: 'aa'}}).find()//and做key
+    // .where({or: [{id: 1, name: {or: [{name: 'aa'}, {memo: 'aa'}]}}, {profile: 'aa'}]}).find()//or嵌套
+    // .where({in: {id: [1,2,3], num: [2,3]}}).find()//in做key
+    // .where({'operator': {id: {'<>': 1, '>=': 0}}}).find()//operator做key
+    // .select({field: 'id', limit: 1, order: {id: 'desc'}, where: {name: {'<>': '', not: 'aa', notin: ['aa', 'rrr'], like: '%a'}}}) //options高级用法
 
-// .where({id: {'<>': 1, '>=': 2, '>': 0,'<': 100, '<=': 10}}).alias('test').select()
-// .countSelect()
-// .join([{from: 'Profile', alias: 'pfile', on: {profile: 'id'}, field: ['id as aid', 'test'], type: 'left'}]).find({field: ['id']})
-// .field(['id','name']).join([{from: 'Profile', on: {or: [{profile: 'id'}, {name: 'test'}], profile: 'id'}, field: ['id', 'test'], type: 'left'}]).countSelect({field: ['name', 'num']})
-//     .select({field: ['id','name'], join: [{from: 'Profile', on: {or: [{profile: 'id'}, {name: 'test'}], profile: 'id'}, field: ['Profile.id as pid', 'test'], type: 'left'}]})
-// .field(['id', 'name']).where({id: {'>=': 0}}).group('name').countSelect()
-// .rel(true, {Profile: {field: ['test']}, Pet: {field: ['types']}, Group: {field: ['name']}}).find()
-// .rel('Profile').find()
-// .where({id: 1}, {name: 'aa'}).count()
-// .add([{name: "test11111111"},{name: "test22"}])
-// .query('select * from think_user where id = 1')
-.where({id:1}).increment('num', 1)
+    // .where({id: {'<>': 1, '>=': 2, '>': 0,'<': 100, '<=': 10}}).alias('test').select()
+    // .countSelect()
+    // .join([{from: 'Profile', alias: 'pfile', on: {profile: 'id'}, field: ['id as aid', 'test'], type: 'left'}]).find({field: ['id']})
+    // .field(['id','name']).join([{from: 'Profile', on: {or: [{profile: 'id'}, {name: 'test'}], profile: 'id'}, field: ['id', 'test'], type: 'left'}]).countSelect({field: ['name', 'num']})
+    //     .select({field: ['id','name'], join: [{from: 'Profile', on: {or: [{profile: 'id'}, {name: 'test'}], profile: 'id'}, field: ['Profile.id as pid', 'test'], type: 'left'}]})
+    // .field(['id', 'name']).where({id: {'>=': 0}}).group('name').countSelect()
+    // .rel(true, {Profile: {field: ['test']}, Pet: {field: ['types']}, Group: {field: ['name']}}).find()
+    // .rel('Profile').find()
+    // .where({id: 1}, {name: 'aa'}).count()
+    // .add([{name: "test11111111"},{name: "test22"}])
+    // .query('select * from think_user where id = 1')
+    .where({ id: 1 }).increment('num', 1)
     // .where({id:1}).decrement('num', 1)
 
-// .add({name: 'qqqesddfsdqqq'})
+    // .add({name: 'qqqesddfsdqqq'})
 
-.then(ress => {
-    echo(`${Date.now() - now}ms -- ${JSON.stringify(ress)}`);
-    process.exit();
-}).catch(e => {
-    // echo(e);
-    process.exit();
-});
+    .then(ress => {
+        echo(`${Date.now() - now}ms -- ${JSON.stringify(ress)}`);
+        process.exit();
+    }).catch(e => {
+        // echo(e);
+        process.exit();
+    });
 
 
 //事务测试
